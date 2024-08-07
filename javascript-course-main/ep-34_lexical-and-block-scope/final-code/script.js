4ff1e25c271c98ba8f19ae61794582706ef6e9db
rkishanCodes <rkishan.codes@gmail.com>
2024-08-07 09:46:43 +0530

    changes done

--- javascript-course-main/ep-34_lexical-and-block-scope/final-code/script.js
+++ javascript-course-main/ep-34_lexical-and-block-scope/final-code/script.js
@@ -1,8 +1,9 @@
-'use strict'
 
-const username = 'Anurag'
-let userAge = 25
-var a = 50
+
+const username = "Anurag";
+let userAge = 25;
+var a = 50;
+num2 = 987;
 
 // function add() {
 //   const username = 'Akash'
@@ -12,42 +13,61 @@ var a = 50
 //   console.log(username)
 // }
 
-debugger
+// debugger;
 
 function subtract() {
-  const x = 15
-  const y = 18
-  const z = 28
+  const x = 15;
+  const y = 18;
+  const z = 28;
   // console.log(x - y)
   // console.log(username)
 
   function child() {
-  // debugger
+    // debugger
 
-    const childName = 'Golu'
+    console.log(num3);
+
+    const childName = "Golu";
     // console.log(childName);
     // console.log(x,z);
 
-    if(true){
-      let num1 = 78
-      var num2 = 987
+    if (true) {
+      let num1 = 78;
       console.log(num1);
-      console.log(num2);
+      // console.log(num2);
+
+      if (true) {
+        var num3 = 12;
+        console.log(num3);
+      }
     }
-    console.log(num2);
+    console.log(num3);
 
     function grandChild() {
-      const grandChildName = 'Molu'
+      const grandChildName = "Molu";
+      console.log(num3);
       // console.log(grandChildName);
     }
-    grandChild()
+    grandChild();
   }
 
-  child()
-
+  child();
 }
 
 // add()
-subtract()
+subtract();
 
-console.log('Program Ended')
+console.log("Program Ended");
+
+function ch() {
+  var x = 0;
+
+  function ch1() {
+    var x = 1;
+    console.log(x);
+  }
+  console.log(x);
+  ch1();
+}
+
+ch();
--- javascript-course-main/ep-35_higher-order-functions/final-code/script.js
+++ javascript-course-main/ep-35_higher-order-functions/final-code/script.js
@@ -1,13 +1,52 @@
-//higher order function
-function a(b) {
-    console.dir(b);
-    b()
+// //higher order function
+// function a(b) {
+//   console.dir(b);
+//   b();
+// }
+
+// //callback function
+// a(ch);
+
+// function ch() {
+//   console.log("Hello World");
+// }
+
+// // function multiplier(factor) {
+// //   return function (number) {
+// //     console.log(factor);
+// //     return number * factor;
+// //   };
+// // }
+
+// // const double = multiplier(2);
+// // console.log(double(5)); // Output: 10
+
+// const person = {
+//   name: "Alice",
+//   age: 30,
+//   address: {
+//     street: "123 Main St",
+//     city: "Wonderland",
+//     country: "Fantasy",
+//   },
+//   hobbies: ["reading", "gardening", "biking"],
+// };
+
+// console.dir(person);
+
+function x() {
+  const a = 0;
+
+  let b = 1;
+
+  function child() {
+    console.log(b);
+    console.log(a);
+  }
+
+  return child;
 }
 
+const y = x();
 
-
-
-//callback function
-a(function() {
-    console.log('Hiiiiiiiiiiiiiiiiiii');
-})
+console.dir(y);
