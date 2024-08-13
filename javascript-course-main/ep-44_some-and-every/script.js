ee87985a03f048cb0bf40ed45ae2780dd7a35e5a
rkishanCodes <rkishan.codes@gmail.com>
2024-08-13 13:54:10 +0530

    changes done

--- javascript-course-main/ep-44_some-and-every/script.js
+++ javascript-course-main/ep-44_some-and-every/script.js
@@ -1,7 +1,7 @@
 const evenNumbers = [2,61, 2, 10, 4, 4, 8];
 
 const result = evenNumbers.some((num, i) => {
-  if (num % 2 === 1) {
+  if (num % 2 === 0) {
     console.log(i);
   }
   return num % 2 === 1;
