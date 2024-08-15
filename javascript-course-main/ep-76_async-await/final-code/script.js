f2354f64f34eb9cc78490fde496336f840ef00cd
rkishanCodes <rkishan.codes@gmail.com>
2024-08-15 23:27:09 +0530

    changes done

--- javascript-course-main/ep-76_async-await/final-code/script.js
+++ javascript-course-main/ep-76_async-await/final-code/script.js
@@ -1,10 +1,35 @@
+console.log("start");
+
 async function makeAsyncRequest() {
-  const url = 'https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com/?sleep=2000'
+  // const url = "https://hub.dummyapis.com/delay?seconds=1";
+  const url = "https://dummyjson.com/products";
   const response = await fetch(url)
-  const data = await response.json()
-  return data
+
+  console.log(response);
+  const data=await response.json();
+  console.log(data);
+  // const data = await response.json()
+  // return data
+
+  console.log("in function");
 }
 
-makeAsyncRequest().then((data) => {
-    console.log(data);
-})
+// makeAsyncRequest().then((data) => {
+//     console.log(data);
+// })
+
+console.log(makeAsyncRequest());
+
+console.log("end");
+
+// console.log("start");
+// fetch("https://hub.dummyapis.com/delay?seconds=2").then((res) =>
+//   console.log(res)
+// );
+// // .then(console.log);
+
+// fetch("https://hub.dummyapis.com/delay?seconds=1").then((res) =>
+//   console.log(res)
+// );
+// // .then(console.log);
+// console.log("end");
