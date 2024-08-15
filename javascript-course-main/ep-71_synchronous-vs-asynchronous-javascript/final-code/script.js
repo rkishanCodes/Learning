cc83a2212f23e2aaf0d9477c769f38e3bab21040
rkishanCodes <rkishan.codes@gmail.com>
2024-08-15 20:15:01 +0530

    changes done

--- javascript-course-main/ep-71_synchronous-vs-asynchronous-javascript/final-code/script.js
+++ javascript-course-main/ep-71_synchronous-vs-asynchronous-javascript/final-code/script.js
@@ -1,5 +1,5 @@
 const image = document.querySelector('img')
-const button = document.querySelector('button')
+const button = document.querySelector('button') 
 
 button.addEventListener('click', () => {
   const xhr = new XMLHttpRequest()
@@ -23,13 +23,13 @@ button.addEventListener('click', () => {
 
 
 
-// const blockingBtn = document.querySelector('.blocking-btn')
+const blockingBtn = document.querySelector('.blocking-btn')
 
-// blockingBtn.addEventListener('click', () => {
-//   const startTime = Date.now()
-//   let currentTime = startTime
+blockingBtn.addEventListener('click', () => {
+  const startTime = Date.now()
+  let currentTime = startTime
 
-//   while (startTime + 4000 > currentTime) {
-//     currentTime = Date.now()
-//   }
-// })
+  while (startTime + 4000 > currentTime) {
+    currentTime = Date.now()
+  }
+})
--- javascript-course-main/ep-73_promises/final-code/script.js
+++ javascript-course-main/ep-73_promises/final-code/script.js
@@ -1,24 +1,87 @@
-const resolveBtn = document.querySelector('#resolve-btn')
-const rejectBtn = document.querySelector('#reject-btn')
+// const resolveBtn = document.querySelector("#resolve-btn");
+// const rejectBtn = document.querySelector("#reject-btn");
+
+// setTimeout(() => {
+//   console.log("timeut");
+// });
 
 const p = new Promise((resolve, reject) => {
-  resolveBtn.addEventListener('click', () => {
-    resolve('Promise Resolveddd')
-  })
 
-  rejectBtn.addEventListener('click', () => {
-    reject('Promise Rejected')
-  })
-})
+  console.log("first promise started");
+  // resolveBtn.addEventListener('click', () => {
+  //   resolve('Promise Resolveddd')
+  // })
+  // rejectBtn.addEventListener('click', () => {
+  //   reject('Promise Rejected')
+  // })
+  // setTimeout(() => {
+  //   resolve("res");
+  // });
+  resolve("res1");
 
-p.then((data) => {
+    console.log("first promise ended");
+
+});
+const p2 = new Promise((resolve, reject) => {
+      console.log("second promise staretd");
+
+  // resolveBtn.addEventListener('click', () => {
+  //   resolve('Promise Resolveddd')
+  // })
+  // rejectBtn.addEventListener('click', () => {
+  //   reject('Promise Rejected')
+  // })
+  // setTimeout(() => {
+  //   resolve("res");
+  // });
+  resolve("res2");
+
+        console.log("second promise ended");
+
+});
+
+// p.then((data) => {
+//   console.log(data);
+//   return 155
+// }).then((data) => {
+//   console.log(data);
+//   return 'Anurag'
+// }).then((data) => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+// })
+
+const result =p.then((data) => {
   console.log(data);
-  return 155
-}).then((data) => {
+  return "return of then"
+});
+
+console.log("result of p then", result);
+console.log("p", p);
+p2.then((data) => {
   console.log(data);
-  return 'Anurag'
-}).then((data) => {
-  console.log(data);
-}).catch(err => {
-  console.log(err);
-})
+});
+
+// console.log(1);
+
+// console.log(2);
+
+// setTimeout(() => {
+//   console.log("settimeout");
+//   for (let i = 0; i < 2; i++) {
+//     console.log("first",i);
+//   }
+// },2000);
+
+// console.log(3);
+// setTimeout(() => {
+//   console.log("settimeout");
+//   for (let i = 0; i < 2; i++) {
+//     console.log("second", i);
+//   }
+// },1000);
+// console.log(4);
+
+
+console.log("end");
