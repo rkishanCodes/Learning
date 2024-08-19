5dd6f2b98ec1c10edd67bbd8a99ec8ddb808bc0a
rkishanCodes <rkishan.codes@gmail.com>
2024-08-19 19:46:23 +0530

    changes done

--- javascript-course-main/ep-80_oop-and-factory-functions/final-code/script.js
+++ javascript-course-main/ep-80_oop-and-factory-functions/final-code/script.js
@@ -3,16 +3,37 @@ function createUser(firstName, lastName, age) {
     firstName,
     lastName,
     age,
-    getAgeYear () {
-      return new Date().getFullYear() - user.age
+    getAgeYear() {
+      return new Date().getFullYear() - user.age;
     },
-  }
-  return user
+  };
+  return user;
 }
 
+const user1 = createUser("Aman", "Mishra", 32);
+const user2 = createUser("Anurag", "Singh", 72);
 
-const user1 = createUser('Aman', 'Mishra', 32)
-const user2 = createUser('Anurag', 'Singh', 72)
+const arr1 = [1, 2];
+const arr2 = [3, 4];
 
-const arr1 = [1,2]
-const arr2 = [3, 4]
+class a {
+  namew = "noise";
+  constructor(name) {
+    this.name = name;
+    console.log("object", this.name);
+  }
+
+  speak() {
+    console.log("I'm speaking", namew);
+  }
+}
+
+obj = new a("kiki");
+
+const ch = {
+  "#age": 34,
+};
+
+
+
+console.log(document.querySelector('li').innerText);
